4225167a783ad1598049d2caaa454d13714e8069
tobiaszgithub <tobiasz.github@gmail.com>
2020-02-06 16:51:53 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 npm install abaplint -g
+pwd
 cd /github/workspace
+pwd
+ls
 abaplint -f total --outformat json --outfile /result.json
 cd /
 npm install @octokit/rest@16.10.0 --loglevel=error
